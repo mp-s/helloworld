1c928c8039ab7fc094431795054529e1c8eaea20
mp_s <mp-s@users.noreply.github.com>
2017-03-09 15:43:50 +0800

    update readme.md

--- test.py
+++ test.py
@@ -2,14 +2,5 @@
 
 import os,sys,codecs
 
-def foo(arg1,arg2,arg3='abc',arg4=4):
-    print arg1
-    print '-'*20
-    print arg2
-    print '-'*20
-    print arg3
-    print '-'*20
-    print arg4
-
-if __name__ == "__main__":
-    foo('fuckl', ['adw'])
+te = raw_input('asdad').split()
+print te
